2d6cc1380d2a5cbe2e3ea39a4e92b733b3c05364
陈督 <1421027452@qq.com>
2019-07-08 08:35:54 +0800

    修改 v7.8 项目

--- app/cc1_hmi/rtc.c
+++ app/cc1_hmi/rtc.c
@@ -11,15 +11,15 @@
 #include "hal_pwr.h"
 
 #include "datetime.h"
-#include "rtc.h" 
+#include "rtc.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 void NVIC_RTC()
 {
     NVIC_InitTypeDef NVIC_InitStructure;
-    
+
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    
+
     /* Enable the RTC Interrupt */
     NVIC_InitStructure.NVIC_IRQChannel = RTC_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
@@ -30,72 +30,48 @@ void NVIC_RTC()
 
 ////////////////////////////////////////////////////////////////////////////////
 void RTC_IRQHandler(void)
-{		 
-    if (RTC_GetITStatus(RTC_IT_SEC) != RESET) {							
-        RTC_GetTime(&gtp);  
+{
+    if (RTC_GetITStatus(RTC_IT_SEC) != RESET) {
+        RTC_GetTime(&gtp);
     }
-    if(RTC_GetITStatus(RTC_IT_ALR) != RESET) { 
+    if(RTC_GetITStatus(RTC_IT_ALR) != RESET) {
         RTC_ClearITPendingBit(RTC_IT_ALR);
-    } 				  								 
+    }
     RTC_ClearITPendingBit(RTC_IT_SEC);
-    RTC_WaitForLastTask();	  	    						 	   	 
+    RTC_WaitForLastTask();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 bool initRTC(void)
 {
-//        PWR_BackupAccessCmd(ENABLE);
-//        BKP_DeInit();
-//        RCC_LSEConfig(RCC_LSE_ON);
-//        while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET);
-//        RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
-//        RCC_RTCCLKCmd(ENABLE);
-//        RTC_WaitForSynchro();
-//        RTC_WaitForLastTask();
-//        RTC_ITConfig(RTC_IT_SEC, ENABLE);
-//        RTC_WaitForLastTask();
-//        RTC_SetPrescaler(32767); /* RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1) */
-//        RTC_WaitForLastTask();
-//        //NVIC_RTC();
-//        return 0;
-    
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);  
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
     PWR->CR |= PWR_CR_DBP;
-    
-    if (BKP_ReadBackupRegister(BKP_DR1) != 0x5050) { 
-        //exBKP_Init();
+
+    if (BKP_ReadBackupRegister(BKP_DR1) != 0x2018) {
+        PWR_BackupAccessCmd(ENABLE);
+        BKP_DeInit();
+
         RCC_LSEConfig(RCC_LSE_ON);
-        
-        while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET) {
-            return 1;
-        }
-        
-        RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);	
+        while (!RCC_GetFlagStatus(RCC_FLAG_LSERDY));
+        RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
         RCC_RTCCLKCmd(ENABLE);
-        RTC_WaitForLastTask();
+
+        BKP_WriteBackupRegister(BKP_DR1, 0x2018);
+
         RTC_WaitForSynchro();
-        //RTC_ITConfig(RTC_IT_SEC, ENABLE);
         RTC_WaitForLastTask();
-        RTC_EnterConfigMode();
-        RTC_SetPrescaler(32767);
+
+        RTC_SetPrescaler(32767);  ///< RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1)
         RTC_WaitForLastTask();
-        //RTC_Set(2016,1,1,0,0,0);
-        RTC_ExitConfigMode();
-        
-        BKP_WriteBackupRegister(BKP_DR1, 0x5050);
-        //NVIC_RTC();
     }
     else  {
-        RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);	
-        RCC_RTCCLKCmd(ENABLE);
         RTC_WaitForSynchro();
-        //RTC_ITConfig(RTC_IT_SEC, ENABLE);
         RTC_WaitForLastTask();
     }
-    
-    BKP_RTCOutputConfig(BKP_RTCOutputSource_CalibClock);
+
+    //BKP_RTCOutputConfig(BKP_RTCOutputSource_CalibClock);
     RTC_GetTime(&gtp);
-	
+
     return 0; //ok
 }
 
@@ -103,32 +79,32 @@ bool initRTC(void)
 ////////////////////////////////////////////////////////////////////////////////
 void checkRTC()
 {
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
-    
-    PWR->CR |= PWR_CR_DBP;
-    
-    if (BKP_ReadBackupRegister(BKP_DR1) != 0x5AA5)  {
-        BKP_WriteBackupRegister(BKP_DR1, 0x5AA5);
-        
-        PWR_BackupAccessCmd(DISABLE);
-        
-        initRTC();
-    }
-    else {
-        if (RCC_GetFlagStatus(RCC_FLAG_PORRST) != RESET)
-            RTC_WaitForSynchro(); 
-        
-        RTC_ITConfig(RTC_IT_SEC, ENABLE);
-        RTC_WaitForLastTask();
-    }
-    BKP_TamperPinCmd(DISABLE); 
-    BKP_ITConfig(DISABLE);
-    
-    BKP_RTCOutputConfig(BKP_RTCOutputSource_CalibClock);
-    RCC_ClearFlag();
-    
-    timeSel = 0;
-    
+//    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
+//
+//    PWR->CR |= PWR_CR_DBP;
+//
+//    if (BKP_ReadBackupRegister(BKP_DR1) != 0x5AA5)  {
+//        BKP_WriteBackupRegister(BKP_DR1, 0x5AA5);
+//
+//        PWR_BackupAccessCmd(DISABLE);
+//
+//        initRTC();
+//    }
+//    else {
+//        if (RCC_GetFlagStatus(RCC_FLAG_PORRST) != RESET)
+//            RTC_WaitForSynchro();
+//
+//        RTC_ITConfig(RTC_IT_SEC, ENABLE);
+//        RTC_WaitForLastTask();
+//    }
+//    BKP_TamperPinCmd(DISABLE);
+//    BKP_ITConfig(DISABLE);
+//
+//    BKP_RTCOutputConfig(BKP_RTCOutputSource_CalibClock);
+//    RCC_ClearFlag();
+//
+//    timeSel = 0;
+
 }
 
 
