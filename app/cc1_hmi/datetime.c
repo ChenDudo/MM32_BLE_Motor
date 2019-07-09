e3f7d6dada469b63f0805ad35aa79e042b1222b5
chendu-home <chendu@mindmotion.com.cn>
2019-07-09 16:05:56 +0800

    工作时间添加半点提醒

--- app/cc1_hmi/datetime.c
+++ app/cc1_hmi/datetime.c
@@ -20,9 +20,14 @@ void AlarmTick()
         // 9:00 - 18:00 上下班提醒 (5s) 及 开灯
         if(gtp.minute == 0) {
             beepMode = bibi;
-            if(gtp.second > 4)
+            if(gtp.second > 3)
                 beepMode = biNone;
         }
+        if(gtp.minute == 30) {      // 半点提醒 (1s)
+                beepMode = bi;
+                if(gtp.second > 1)
+                    beepMode = biNone;
+            }
         ledCmd = 1;     //open logo led
     }
     else {
